fcb939a43cf0ebbbb87e4e7fbecb369c2d312f7b
julia <julia.garrido@gmail.com>
2026-05-19 22:05:18 -0300

    corrigo index modul que le faltaba el .js

--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,14 @@
-import Playlist from "./playlist";
-import Usuario from "./usuario";
-import Plan from "./plan";
-import Suscripcion from "./suscripcion";
-import Artista from "./artista";
-import Album from "./album";
-import Cancion from "./cancion";
-import PlaylistCancion from "./playlistCancion";
-import Reproduccion from "./reproduccion";
-import ArtistaCancion from "./artistaCancion";
-import ArtistaAlbum from "./artistaAlbum"
+import Playlist from "./playlist.js";
+import Usuario from "./usuario.js";
+import Plan from "./plan.js";
+import Suscripcion from "./suscripcion.js";
+import Artista from "./artista.js";
+import Album from "./album.js";
+import Cancion from "./cancion.js";
+import PlaylistCancion from "./playlistCancion.js";
+import Reproduccion from "./reproduccion.js";
+import ArtistaCancion from "./artistaCancion.js";
+import ArtistaAlbum from "./artistaAlbum.js"
 export {
     Playlist,
     Usuario,
